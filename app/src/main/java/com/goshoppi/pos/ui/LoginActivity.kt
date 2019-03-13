306878f68e0ea0e0b14889456701c27cdac67b9b
MFarhan <mfarhan.rg@gmail.com>
2019-03-13 10:50:58 +0500

    Add : Login Activity and its Resources.

---
+++ app/src/main/java/com/goshoppi/pos/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.goshoppi.pos.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.goshoppi.pos.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/goshoppi/pos/ui/PosMainActivity.kt
+++ app/src/main/java/com/goshoppi/pos/ui/PosMainActivity.kt
@@ -1,4 +1,4 @@
-package com.goshoppi.pos.view
+package com.goshoppi.pos.ui
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
