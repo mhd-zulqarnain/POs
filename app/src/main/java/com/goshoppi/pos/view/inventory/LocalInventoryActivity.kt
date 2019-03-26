6d572e98b6b6a79aef8a6130c7d2c97d44a45f4c
zulqarnain <mhd.zeeq@gmail.com>
2019-03-26 18:08:51 +0500

    issue solved:Work manager scheduling

--- app/src/main/java/com/goshoppi/pos/view/inventory/LocalInventoryActivity.kt
+++ app/src/main/java/com/goshoppi/pos/view/inventory/LocalInventoryActivity.kt
@@ -1,10 +1,12 @@
 package com.goshoppi.pos.view.inventory
 
+import android.annotation.SuppressLint
 import android.arch.lifecycle.Observer
 import android.content.SharedPreferences
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.SearchView
 import android.view.MenuItem
@@ -12,10 +14,12 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.goshoppi.pos.R
 import com.goshoppi.pos.architecture.repository.localProductRepo.LocalProductRepository
+import com.goshoppi.pos.architecture.repository.localVariantRepo.LocalVariantRepository
 import com.goshoppi.pos.di.component.DaggerAppComponent
 import com.goshoppi.pos.di.module.AppModule
 import com.goshoppi.pos.di.module.RoomModule
 import com.goshoppi.pos.model.local.LocalProduct
+import com.goshoppi.pos.model.local.LocalVariant
 import com.goshoppi.pos.utils.Utils
 import com.ishaquehassan.recyclerviewgeneraladapter.RecyclerViewGeneralAdapter
 import com.squareup.picasso.Picasso
@@ -29,9 +33,13 @@ class LocalInventoryActivity : AppCompatActivity(),
     private var currentTheme: Boolean = false
     private lateinit var sharedPref: SharedPreferences
     private var productList: ArrayList<LocalProduct> = ArrayList()
+    private var variantList: ArrayList<LocalVariant> = ArrayList()
 
     @Inject
     lateinit var localProductRepository: LocalProductRepository
+
+    @Inject
+    lateinit var localVariantRepository: LocalVariantRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,7 +65,11 @@ class LocalInventoryActivity : AppCompatActivity(),
         localProductRepository.loadAllLocalProduct().observe(this,
             Observer<List<LocalProduct>> { localProductsList ->
                 productList = localProductsList as ArrayList
-                setUpRecyclerView(productList)
+                setUpPrdouctRecyclerView(productList)
+                if (productList.size != 0) {
+                    getShowVaraint(productList[0].storeProductId)
+                    tv_varaint_prd_name.text = productList[0].productName
+                }
                 rc_product_details_variants.adapter?.notifyDataSetChanged()
             })
 
@@ -72,6 +84,7 @@ class LocalInventoryActivity : AppCompatActivity(),
                 return true
             }
         })
+        variantList = ArrayList()
     }
 
     private fun searchProduct(param: String) {
@@ -84,7 +97,7 @@ class LocalInventoryActivity : AppCompatActivity(),
                 productList = localProductList as ArrayList
 
                 if (productList.size > 0) {
-                    setUpRecyclerView(productList)
+                    setUpPrdouctRecyclerView(productList)
                     rc_product_details_variants.adapter?.notifyDataSetChanged()
                 } else {
                     Utils.showMsg(this, "No result found")
@@ -92,9 +105,10 @@ class LocalInventoryActivity : AppCompatActivity(),
             })
     }
 
-    private fun setUpRecyclerView(list: ArrayList<LocalProduct>) {
+    private fun setUpPrdouctRecyclerView(list: ArrayList<LocalProduct>) {
 
         rc_product_details_variants.layoutManager = LinearLayoutManager(this@LocalInventoryActivity)
+
         rc_product_details_variants.adapter =
             RecyclerViewGeneralAdapter(list, R.layout.single_product_view)
             { itemData, viewHolder ->
@@ -109,6 +123,54 @@ class LocalInventoryActivity : AppCompatActivity(),
                 productItemNewPrice.text = itemData.offerPrice
 
                 val file = Utils.getProductImage(itemData.storeProductId, "1")
+                Timber.e("File is there ? ${file.exists()}")
+
+                mainView.setOnClickListener {
+                    getShowVaraint(itemData.storeProductId)
+                    tv_varaint_prd_name.text = itemData.productName
+                }
+                if (file.exists()) {
+                    Picasso.get()
+                        .load(file)
+                        .error(R.drawable.no_image)
+                        .into(productItemIcon)
+
+                } else {
+                    Picasso.get()
+                        .load(R.drawable.no_image)
+                        .into(productItemIcon)
+                }
+            }
+    }
+
+    private fun getShowVaraint(productId: String) {
+        localVariantRepository.getLocalVariantsByProductId(productId).observe(this,
+            Observer<List<LocalVariant>> { localVariantList ->
+                variantList = localVariantList as ArrayList
+                setUpVaraintRecyclerView(variantList)
+                rc_product_details_variants.adapter?.notifyDataSetChanged()
+            })
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setUpVaraintRecyclerView(list: ArrayList<LocalVariant>) {
+
+        rv_products_variants.layoutManager = GridLayoutManager(this@LocalInventoryActivity, 2)
+
+        rv_products_variants.adapter =
+            RecyclerViewGeneralAdapter(list, R.layout.single_product_view)
+            { itemData, viewHolder ->
+                val mainView = viewHolder.itemView
+                val productItemTitle = mainView.findViewById<TextView>(R.id.product_item_title)
+                val productItemNewPrice = mainView.findViewById<TextView>(R.id.product_item_new_price)
+                val productItemOldPrice = mainView.findViewById<TextView>(R.id.product_item_old_price)
+                val productItemIcon = mainView.findViewById<ImageView>(R.id.product_item_icon)
+
+                productItemTitle.text = "Varaint Id: ${itemData.rangeId}"
+                productItemOldPrice.text = itemData.productMrp
+                productItemNewPrice.text = itemData.offerPrice
+
+                val file = Utils.getVaraintImage(itemData.productId, itemData.storeRangeId)
                 Timber.e("File is there ? ${file.exists()}")
 
                 if (file.exists()) {
