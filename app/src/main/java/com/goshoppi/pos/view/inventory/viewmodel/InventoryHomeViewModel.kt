7d0d902449d2e2e4d140e56ae501e84ea48f0c3c
zulqarnain <mhd.zeeq@gmail.com>
2019-04-23 10:22:39 +0500

    bug solved in removing items in main activity

--- app/src/main/java/com/goshoppi/pos/view/inventory/viewmodel/InventoryHomeViewModel.kt
+++ app/src/main/java/com/goshoppi/pos/view/inventory/viewmodel/InventoryHomeViewModel.kt
@@ -4,8 +4,6 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.paging.LivePagedListBuilder
 import android.arch.paging.PagedList
-import com.goshoppi.pos.architecture.repository.customerRepo.CustomerRepository
-import com.goshoppi.pos.architecture.repository.localProductRepo.LocalProductRepository
 import com.goshoppi.pos.architecture.repository.masterProductRepo.MasterProductRepository
 import com.goshoppi.pos.di2.scope.AppScoped
 import com.goshoppi.pos.model.master.MasterProduct
